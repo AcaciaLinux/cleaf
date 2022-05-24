ed90b75cac9a80f8818b8a9389d35212430d6275
Max Kofler <kofler.max.dev@gmail.com>
2022-05-24 13:28:56 -0700

    Fixed some bugs, added LOGAPI logging

--- src/cleafconfig/cleafconfig.cpp
+++ src/cleafconfig/cleafconfig.cpp
@@ -8,14 +8,17 @@
 
 extern "C"{
 
+	void cleafconfig_setRootDir(const char* dir){
+		FUN();
+		lConfig.setRootDir(std::string(dir));
+		LOGAPI("[cleaf] Setting cleafconfig root directory to " + std::string(dir));
+	}
+
 	void leafconfig_setRedownload(cleaf_config_redownload redownload){
 		FUN();
 
 		lConfig.redownload = (config_redownload)redownload;
-	}
-
-	void cleafconfig_setRootDir(const char* dir){
-		lConfig.setRootDir(std::string(dir));
+		LOGAPI("[cleaf] Setting redownload mode to " + std::to_string((int)redownload));
 	}
 
 	void cleafconfig_setBoolConfig(cleaf_bool_config config, int option){
@@ -43,28 +46,38 @@ extern "C"{
 
 		}
 
+		LOGAPI("[cleaf] Set boolean config " + std::to_string(config) + " to " + std::to_string(state));
+
 	}
 
 	int leafconfig_getBoolConfig(cleaf_bool_config config){
 		FUN();
 
+		int res = -1;
+
 		switch (config){
 		
 		case CLEAF_B_CONFIG_NOASK:
-			return lConfig.noAsk;
+			res = lConfig.noAsk;
+			break;
 
 		case CLEAF_B_CONFIG_FORCEOVERWRITE:
-			return lConfig.forceOverwrite;
+			res = lConfig.forceOverwrite;
+			break;
 
 		case CLEAF_B_CONFIG_RUNPREINSTALL:
-			return lConfig.runPreinstall;
+			res = lConfig.runPreinstall;
+			break;
 
 		case CLEAF_B_CONFIG_RUNPOSTINSTALL:
-			return lConfig.runPostinstall;
+			res = lConfig.runPostinstall;
+			break;
 
 		}
 
-		return 255;
+		LOGAPI("[cleaf] Got state of bool config " + std::to_string(config) + ": " + std::to_string(res));
+
+		return res;
 
 	}
 
@@ -104,6 +117,8 @@ extern "C"{
 			break;
 
 		}
+
+		LOGAPI("[cleaf] Got state of string config " + std::to_string(config) + ": \"" + res + "\"");
 
 		char* resString = new char[res.length()+1];
 		std::strcpy(resString, res.c_str());
--- src/cleafcore/a_install.cpp
+++ src/cleafcore/a_install.cpp
@@ -12,13 +12,13 @@ extern "C" {
 	int cleafcore_a_install(void* cleafcore, int countPackages, char** c_packages){
 		FUN();
 		Leafcore* leaf = (Leafcore*) cleafcore;
-		LOGD("Got " + std::to_string(countPackages) + " packages to install");
+		LOGAPI("[cleaf] Got " + std::to_string(countPackages) + " packages to install");
 
 		std::deque<std::string> packages;
 
 		for (int i = 0; i < countPackages; i++){
 			packages.push_back(std::string(c_packages[i]));
-			LOGD("Got package \"" + packages.back() + "\" at index " + std::to_string(i));
+			LOGAPI("[cleaf] Got package \"" + packages.back() + "\" at index " + std::to_string(i));
 		}
 
 		try{
