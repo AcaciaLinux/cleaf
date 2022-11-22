ad3c4407d940c87860c2ddc7055f1a256895a59d
Max Kofler <kofler.max.dev@gmail.com>
2022-11-22 20:14:51 +0000

    Integrated version checks into a_upgrade.cpp

--- src/cleafcore/a_upgrade.cpp
+++ src/cleafcore/a_upgrade.cpp
@@ -1,5 +1,6 @@
 #include "log.h"
 #include "cleafcore.h"
+#include "leafcore_dist.h"
 
 #include <leafcore/leafcore.h>
 #include <leafcore/error.h>
@@ -11,25 +12,29 @@ extern "C" {
 
 	int cleafcore_a_upgrade(void* cleafcore, int countPackages, char** c_packages){
 		FUN();
-		Leafcore* leaf = (Leafcore*) cleafcore;
-		LOGAPI("[cleaf] Got " + std::to_string(countPackages) + " packages to change");
+	
+		#if CLEAFCORE_LEAFCORE_MINOR >= 2
+			Leafcore* leaf = (Leafcore*) cleafcore;
+			LOGAPI("[cleaf] Got " + std::to_string(countPackages) + " packages to change");
 
-		std::deque<std::string> packages;
+			std::deque<std::string> packages;
 
-		for (int i = 0; i < countPackages; i++){
-			packages.push_back(std::string(c_packages[i]));
-			LOGAPI("[cleaf] Got package \"" + packages.back() + "\" at index " + std::to_string(i));
-		}
+			for (int i = 0; i < countPackages; i++){
+				packages.push_back(std::string(c_packages[i]));
+				LOGAPI("[cleaf] Got package \"" + packages.back() + "\" at index " + std::to_string(i));
+			}
 
-		try{
-			leaf->a_upgrade(packages);
-		} catch (LeafError* e){
-			LOGUE("Change failed: " + e->what());
-			return e->getErrorCode();
-		}
+			try{
+				leaf->a_upgrade(packages);
+			} catch (LeafError* e){
+				LOGUE("Change failed: " + e->what());
+				return e->getErrorCode();
+			}
 
-		return 0;
-
+			return 0;
+		#else
+			return e_feature_missing("a_upgrade", 0, 3);
+		#endif
 	}
 
 }
